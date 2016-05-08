b0ecf5b9a6d31b630333ee103115cd5622e958ba
dloomis <dloomis@gmail.com>
2016-05-07 20:24:40 -0400

    .

--- js/spark.js
+++ js/spark.js
@@ -142,7 +142,7 @@ var cardButtonCallback = function(t){
     return {
       text: sparkButtonMenuItems[sparkMenuCode],
       callback: function(t) {
-        return t.attach({url: "", name: sparkButtonMenuItems[sparkMenuCode] })
+        return t.attach({url: "http://www.google.com", name: sparkButtonMenuItems[sparkMenuCode] })
         .then(function(){
               return t.closePopup();
             })
